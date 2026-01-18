b6347580fe2923aee2cde0e71f451bc067dcdfd6
OscarC <yo_poraki@yahoo.es>
2026-01-18 14:27:56 +0100

    add empty signIn, signUp and NotFound pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { SignUp } from './sign-up/sign-up';
+import { SignIn } from './sign-in/sign-in';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SignIn,
+  },
+  {
+    path: 'SignIn',
+    component: SignIn,
+  },
+  {
+    path: 'SignUp',
+    component: SignUp,
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
+];
